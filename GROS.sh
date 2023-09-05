0d7c0fb1e6acdce9edd78a9f301f7ade7c663872
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-09-06 03:20:04 +0530

    Create OS Folder

--- GROS.sh
+++ GROS.sh
@@ -1,4 +1,12 @@
+# Entering OS
 printf '%s\n' "GROS - Ghost Reborn OS"
 printf '%s' "Enter name of OS: "
 read OS_NAME
 printf '%s%s\n' "OS Name: " $OS_NAME
+
+# Creating Folder
+OS_PWD="/home/"$(whoami)"/"
+printf '%s' $OS_PWD
+cd $OS_PWD
+mkdir -p $OS_NAME
+cd $OS_NAME
