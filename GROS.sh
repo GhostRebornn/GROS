fd8000243156238199a991e347efc2568527c6b4
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-09-06 03:30:08 +0530

    Sync AOSP Sources

--- GROS.sh
+++ GROS.sh
@@ -6,7 +6,19 @@ printf '%s%s\n' "OS Name: " $OS_NAME
 
 # Creating Folder
 OS_PWD="/home/"$(whoami)"/"
-printf '%s' $OS_PWD
 cd $OS_PWD
 mkdir -p $OS_NAME
 cd $OS_NAME
+
+# Syncing AOSP Sources
+printf "Enter Git username: "
+read GIT_USERNAME
+printf "Enter Git email: "
+read GIT_EMAIL
+
+git config --global credential.helper store
+git config --global user.name $GIT_USERNAME
+git config --global user.email $GIT_EMAIL
+
+repo init --depth=1 --groups=all,-darwin -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r74
+repo sync -j2 -c -v --force-sync --no-clone-bundle --no-tags
