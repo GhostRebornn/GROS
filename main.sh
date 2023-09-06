764ac0a40d74d85d37dab1fe0062fbd90a22b6dd
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-09-06 17:19:33 +0530

    Resync sources if not found

--- main.sh
+++ main.sh
@@ -12,6 +12,11 @@ function isTrue() {
 function getOSDirectory() {
   read -p "Enter OS Directory like /home/user/some/os/path: " OS_PATH
   cd $OS_PATH
+  printf "Resync sources: \n"
+  if isTrue; then
+    repo init --depth=1 --groups=all,-darwin -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r74
+    repo sync -j2 -c -v --force-sync --no-clone-bundle --no-tags
+  fi
 }
 
 printf "Synced AOSP Sources: \n"
