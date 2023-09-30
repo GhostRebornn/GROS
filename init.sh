64fae0c40c78f9fa5bed7484e6436f06ce6c670c
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-09-30 18:14:27 +0530

    Update Revision

--- init.sh
+++ init.sh
@@ -20,5 +20,5 @@ git config --global credential.helper store
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 
-repo init --depth=1 --groups=all,-darwin -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r74
+repo init --depth=1 --groups=all,-darwin -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r82
 repo sync -j2 -c -v --force-sync --no-clone-bundle --no-tags
