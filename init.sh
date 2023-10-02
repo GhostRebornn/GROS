22d09b019f51438dbd64264a579ab46f04b9aedd
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-02 06:43:10 +0530

    Convert snippets linking as function * Replace fetch=".." -> fetch="android.googlesource.com" * Add <include name="snippets/GROS.xml" /> only if not present

--- init.sh
+++ init.sh
@@ -21,10 +21,36 @@ git config --global credential.helper store
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 
+link_snippets() {
+    cp -r $CUR_DIR/snippets $OS_PWD$OS_NAME/.repo/manifests/
+    cd $OS_PWD$OS_NAME/.repo/manifests
+    sed 's/fetch=".."/fetch="android.googlesource.com"/' default.xml > temp.txt && mv temp.txt default.xml
+    awk '
+        /<include name="snippets\/GROS.xml" \/>/ {
+            found = 1
+        }
+        {
+            a[i++] = $0
+        }
+        END {
+            if (!found) {
+                i--
+            }
+            for (j = 0; j < i; j++) {
+                print a[j]
+            }
+        }
+        END {
+            if (!found) {
+                print "  <include name=\"snippets/GROS.xml\" />"
+                print "</manifest>"
+            }
+        }
+        ' default.xml > temp.txt && mv temp.txt default.xml
+    cd ..
+    cd ..
+}
+
 repo init --depth=1 --groups=all,-darwin -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r82
-cp -r $CUR_DIR/snippets $OS_PWD$OS_NAME/.repo/manifests/
-cd $OS_PWD$OS_NAME/.repo/manifests
-awk '{a[i++]=$0} END{for (j=0; j<i-1; j++) print a[j] } END{print "  <include name=\"snippets/GROS.xml\" />\n</manifest>"}' default.xml > temp.txt && mv temp.txt default.xml
-cd ..
-cd ..
+link_snippets
 repo sync -j2 -c -v --force-sync --no-clone-bundle --no-tags
